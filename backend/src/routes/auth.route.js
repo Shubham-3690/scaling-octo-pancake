4101f873d988b71127daf9d710451492e832a85b
Shubham-3690 <Shubham511515@gmail.com>
2025-02-10 05:27:23 +0530

    Update auth.route.js

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,15 +1,21 @@
 import express from "express";
-import { checkAuth, login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
+import { 
+  checkAuth, 
+  login, 
+  logout, 
+  signup, 
+  updateProfile 
+} from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
-router.post("/logout", logout);
+router.post("/logout", protectRoute, logout); // Protect logout route
 
 router.put("/update-profile", protectRoute, updateProfile);
 
-router.get("/check", protectRoute, checkAuth);
+router.get("/check", protectRoute, checkAuth); // Ensures auth status
 
 export default router;
